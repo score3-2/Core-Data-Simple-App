4d5756e2206944da709e11ec6e2b5d77f6a585ec
Abel P <abelomar@icloud.com>
2018-09-19 20:40:12 -0700

    Initial Commit

---
+++ Core Data By RWTests/Core_Data_By_RWTests.swift
@@ -0,0 +1,34 @@
+//
+//  Core_Data_By_RWTests.swift
+//  Core Data By RWTests
+//
+//  Created by zero on 9/19/18.
+//  Copyright © 2018 Abel C. All rights reserved.
+//
+
+import XCTest
+@testable import Core_Data_By_RW
+
+class Core_Data_By_RWTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
